33bb3741862c9f16211f513024efedd6964fe35f
yinyansheng <yinyansheng@jiangduoduo.com>
2020-03-26 21:29:31 +0800

    [*] m

---
+++ src/main/java/com.donne.leetcode/L01_tow_sum.java
@@ -0,0 +1,40 @@
+package com.donne.leetcode;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/two-sum/
+ */
+public class L01_tow_sum {
+    public static void main(String[] args) {
+        int[] ints = twoSum(new int[]{3, 2, 4}, 6);
+        System.out.println(JSON.toJSONString(ints));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        if (null == nums || 0 == nums.length) {
+            return null;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            if (map.containsKey(left) && map.get(left) != i) {
+                return new int[]{i, map.get(left)};
+            }
+        }
+
+        return null;
+    }
+
+
+}
