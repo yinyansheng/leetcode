25141872ce891a288a1d2bdd11575f49b7dccb29
yinyansheng <yinyansheng@jiangduoduo.com>
2020-04-07 20:54:01 +0800

    m

---
+++ src/main/java/com.donne.leetcode/ClimbStairsSolution.java
@@ -0,0 +1,36 @@
+package com.donne.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbStairsSolution {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(10));
+    }
+
+
+    public static int climbStairs(int n) {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        return climbStairs(n, resultMap);
+    }
+
+    public static int climbStairs(int n, Map<Integer, Integer> resultMap) {
+        if (resultMap.containsKey(n)) {
+            return resultMap.get(n);
+        }
+
+        int result;
+        if (n == 1) {
+            result = 1;
+        } else if (n == 2) {
+            result = 2;
+        } else {
+            result = climbStairs(n - 1, resultMap) + climbStairs(n - 2, resultMap);
+        }
+
+        resultMap.put(n, result);
+        return result;
+    }
+
+
+}
