15fcca9c3309d17ea1c9018509164eea648222ad
yinyansheng <yinyansheng@jiangduoduo.com>
2020-04-12 21:12:35 +0800

    m

---
+++ src/main/java/com.donne.leetcode/Solution20.java
@@ -0,0 +1,43 @@
+package com.donne.leetcode;
+
+import java.util.Stack;
+
+/**
+ *
+ */
+class Solution20 {
+
+    public static void main(String[] args) {
+        String input = "{[()]}a";
+        System.out.println(isValid(input));
+    }
+
+    public static boolean isValid(String input) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+            if (!stack.isEmpty() && judge(stack.peek(), c)) {
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+
+        return stack.isEmpty();
+    }
+
+    private static boolean judge(char a1, char a2) {
+        if (a1 == '(' && a2 == ')') {
+            return true;
+        }
+
+        if (a1 == '[' && a2 == ']') {
+            return true;
+        }
+
+        if (a1 == '{' && a2 == '}') {
+            return true;
+        }
+        return false;
+    }
+}
