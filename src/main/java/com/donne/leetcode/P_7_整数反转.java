a1cb7fc525c8ce5dc6e8884662eb003d239b220a
yinyansheng <donne@foxmail.com>
2020-04-23 23:25:10 +0800

    m

--- src/main/java/com/donne/leetcode/P_7_整数反转.java
+++ src/main/java/com/donne/leetcode/P_7_整数反转.java
@@ -3,14 +3,10 @@ package com.donne.leetcode;
 public class P_7_整数反转 {
 
     public static void main(String[] args) {
-        System.out.println(reverse(0, -2147483648));
+        System.out.println(reverse(0, 123));
     }
 
     public static int reverse(int res, int x) {
-        if (res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10) {
-            return 0;
-        }
-
         if (Math.abs(x) < 10) {
             return res * 10 + x;
         }
