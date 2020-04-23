8c51f19c5265760ca30068fe3c34ea8a417adda2
yinyansheng <donne@foxmail.com>
2020-04-23 14:46:38 +0800

    m

--- src/main/java/com/donne/offer/P_1_字符串的排列.java
+++ src/main/java/com/donne/offer/P_1_字符串的排列.java
@@ -1,14 +1,36 @@
 package com.donne.offer;
 
-import java.util.List;
-
 public class P_1_字符串的排列 {
     public static void main(String[] args) {
-
+        permutation("abcd".toCharArray(), 0, 3);
     }
 
-    private static String permutation(String input, int start) {
-        return "";
+
+    /**
+     * abc
+     * <p>
+     * a bc
+     * b ac
+     * c ab
+     */
+    private static void permutation(char[] chars, int from, int to) {
+        if (from == to) {
+            System.out.println(chars);
+            return;
+        }
+
+        for (int i = from; i <= to; i++) {
+            swap(chars, from, i);
+            permutation(chars, from + 1, to);
+            swap(chars, from, i);
+        }
+    }
+
+
+    private static void swap(char[] chars, Integer from, Integer to) {
+        char tmp = chars[from];
+        chars[from] = chars[to];
+        chars[to] = tmp;
     }
 
 
