cb02c3912bbf6797f550d6e9da854f2f9d18df28
yinyansheng <yinyansheng@jiangduoduo.com>
2020-08-04 14:55:15 +0800

    m

---
+++ src/main/java/com/donne/jvm/AA.java
@@ -0,0 +1,13 @@
+package com.donne.jvm;
+
+public class AA {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/donne/jvm/ClassLoaderTraceDemo.java
@@ -0,0 +1,12 @@
+package com.donne.jvm;
+
+public class ClassLoaderTraceDemo {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+
+        AA aa = new AA();
+        aa.setName("aa");
+
+        System.out.println("2222222");
+    }
+}
